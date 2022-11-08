6ce99f90a037814475ae544ff959b2b3565a1609
1 <a1@1s-MacBook-Air.local>
2022-11-08 12:42:56 +0200

    test

---
+++ main.js
@@ -0,0 +1,10 @@
+function getFirstLetter (word){
+    let string = word;
+    if (string == Number){
+        console.log(string.match(/[0-9]/))
+    }
+    else if (string = String) {
+        console.log(string.match(/a-b/))
+    }
+}
+getFirstLetter("a-b")
